90bb76a24d10482ea6bc711b1668a2d0aefd870c
Anshika gupta <anshikagupta1318@gmail.com>
2026-02-02 12:09:40 +0530

    Correct event handling and variable references
    Fix variable names and correct innerHTML usage.

--- x.js
+++ x.js
@@ -2,14 +2,20 @@
   let form =document.querySelector('form')
   let evenTtitle=document.querySelector('#eventTitle')
   let eventDate=document.querySelector('#eventDate')
-let eventCategory=document.querySelector('#eventCategory')
-let eventDescription=document.querySelector('#eventDescription')
-let allEvenrContainer=document.querySelector('#allEventContainer')
+  let eventCategory=document.querySelector('#eventCategory')
+  let eventDescription=document.querySelector('#eventDescription')
+  let allEvenrContainer=document.querySelector('#allEventContainer')
 
 function addEventFuction(event){
   event.preventDefault()
   let card = document.createElement('div')
   card.innerhtml=<button></button>
-  
-  
+                  <h4>$(eventTitle.value)
+                  <span>$(eventDate.value)</span><br>
+                    <span>$(eventCategory.value)<span>
+                    <p>$(eventDescritption.value)</p>
+  allEventContainer.append(card);
+  form.reset()
+}
 form.addEventListener('submit',addEventFunction)
+  </script>
