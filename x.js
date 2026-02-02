ee4217e3e6b71468f8b1759f86361acd83f3c722
Anshika gupta <anshikagupta1318@gmail.com>
2026-02-02 11:43:07 +0530

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,8 @@
+<script>
+  let form =document.querySelector('form')
+function addEventFuction(event){
+  event.preventDefault()
+  console.log(eventTitle.value)
+  
 
+form.addEventListener('submit',addEventFunction)
